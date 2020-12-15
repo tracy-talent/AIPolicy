f3d1e59bc3ab936314edab5699bba120727756d1
mecthew <905843145@qq.com>
2020-12-15 22:51:33 +0800

    feat(test_inference): add sentence_importance classifier

--- pasaie/pasaap/pretrain.py
+++ pasaie/pasaap/pretrain.py
@@ -12,10 +12,10 @@ config.read(os.path.join(project_path, 'config.ini'))
 default_root_path = config['path']['ap_ckpt']
 
 
-def get_model(dataset_name,
-              model_name,
+def get_model(model_path,
               root_path=default_root_path):
-    abs_ckpt_path = os.path.join(root_path, dataset_name, model_name + '.pth.tar')
+    dataset_name = model_path.split('/')[0]
+    abs_ckpt_path = os.path.join(root_path, model_path + '.pth.tar')
     if dataset_name in ['sentence_importance_judgement']:
         sij_model = torch.load(abs_ckpt_path, map_location='cpu')
         return sij_model
--- test/test_inference.py
+++ test/test_inference.py
@@ -1,12 +1,14 @@
 import sys
+
 sys.path.append('..')
 import pasaie
-from pasaie import pasaner, pasare
+from pasaie import pasaner, pasare, pasaap
 import os
 import six
 import configparser
 
 import unittest
+
 
 class TestInference(unittest.TestCase):
 
@@ -15,12 +17,13 @@ class TestInference(unittest.TestCase):
         project_path = '/'.join(os.path.abspath(__file__).split('/')[:-2])
         self.config = configparser.ConfigParser()
         self.config.read(os.path.join(project_path, 'config.ini'))
-        self.entity_model_path_list = ['policy_bmoes/bert_lstm_crf0', 
-                            'policy_bmoes/bert_lstm_mrc_dice_fgm0',
-                            'policy_bmoes/multi_bert_dice_fgm0',
-                            'policy_bmoes/single_bert_dice_fgm0']
-        self.relation_model_path_list = ['test-policy/bert_entity_dsp_dice_fgm_attention_cat_test0', 
-                            'test-policy/bert_entity_dice_alpha0.6_fgm0']
+        self.entity_model_path_list = ['policy_bmoes/bert_lstm_crf0',
+                                       'policy_bmoes/bert_lstm_mrc_dice_fgm0',
+                                       'policy_bmoes/multi_bert_dice_fgm0',
+                                       'policy_bmoes/single_bert_dice_fgm0']
+        self.relation_model_path_list = ['test-policy/bert_entity_dsp_dice_fgm_attention_cat_test0',
+                                         'test-policy/bert_entity_dice_alpha0.6_fgm0']
+        self.sentence_importance_path_list = ['sentence_importance_judgement/base_textcnn_ce_fgm1']
 
     def test_policy_bmoes_bert_crf(self):
         query_path = os.path.join(self.config['path']['ner_dataset'], 'policy', 'query.txt')
@@ -28,15 +31,18 @@ class TestInference(unittest.TestCase):
             query_dict = dict(line.strip().split() for line in qf)
         entity_model_path = self.entity_model_path_list[3]
         relation_model_path = self.relation_model_path_list[1]
+        sentence_importance_path = self.sentence_importance_path_list[0]
         # entity_model = pasaie.pasaner.get_model(entity_model_path[0])
+        # relation_model = pasaie.pasare.get_model(relation_model_path[1])
         entity_model = pasaie.pasaner.get_model(entity_model_path)
         relation_model = pasaie.pasare.get_model(relation_model_path)
-        # relation_model = pasaie.pasare.get_model(relation_model_path[1])
+        sentence_importance_model = pasaie.pasaap.get_model(model_path=sentence_importance_path)
         while True:
             if six.PY3:
                 text = input().encode('utf-8', errors='ignore').decode('utf-8', errors='ignore')
             elif six.PY2:
                 text = input().decode('utf-8', errors='ignore').encode('utf-8', errors='ignore')
+            print("text is importance? {}".format(sentence_importance_model.infer(text)))
             if 'mrc' in entity_model_path:
                 tokens, entities = entity_model.infer(text, query_dict)
             else:
@@ -46,11 +52,11 @@ class TestInference(unittest.TestCase):
             relations = set()
             for i in range(len(entities)):
                 for j in range(i + 1, len(entities)):
-                    item = {'token':tokens, 'h': {'pos':entities[i][0], 'entity': entities[i][1]},
-                            't': {'pos':entities[j][0], 'entity': entities[j][1]}}
+                    item = {'token': tokens, 'h': {'pos': entities[i][0], 'entity': entities[i][1]},
+                            't': {'pos': entities[j][0], 'entity': entities[j][1]}}
                     relation_type, score = relation_model.infer(item)
                     if relation_type != 'Other':
-                        relations.add((entities[i], relation_type, entities[j])) # 基于mrc的实体抽取会出现实体对应多个类型
+                        relations.add((entities[i], relation_type, entities[j]))  # 基于mrc的实体抽取会出现实体对应多个类型
             for entity1, relation, entity2 in relations:
                 print(f'{entity1} -> {entity2}: {relation}')
 
